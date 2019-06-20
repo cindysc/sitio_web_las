df25fc08f00855c2f39775003d063c5ded9ef5f6
Cindy <cindy.sanhueza2011@gmail.com>
2019-06-20 02:28:36 -0400

    actualización imágenes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var swiper = new Swiper('.swiper-container', {
       slidesPerView: 1,
       spaceBetween: 30,
+      autoHeight: true,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -11,34 +12,3 @@ $(document).ready(function(){
     });
 
 });
-/*
-$ ( function () {
-   ' use strict ' ;
-   var options = {
-    prefetch :  verdadero ,
-    cacheLength :  2 ,
-    onStart : {
-      duración :  250 , // Duración de nuestra animación
-      render :  function ( $ container ) {
-         // Agrega tu animación CSS invirtiendo class
-        $ container . addClass ( ' is-exiting ' );
-
-        // Reinicia tu animación
-        smoothState . restartCSSAnimations ();
-      }
-    }
-    onReady : {
-      duración :  0 ,
-       render :  function ( $ container , $ newContent ) {
-         // Elimina tu animación CSS invirtiendo class
-        $ container . removeClass ( ' is-exiting ' );
-
-        // Inyectar el nuevo contenido
-        $ contenedor . html ($ newContent);
-
-      }
-    }
-  }
-  smoothState =  $ ( ' #main ' ). smoothState (opciones). datos ( ' smoothState ' );
-});
-*/
